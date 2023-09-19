224531288e10e7755bf34315356874683ec3b05b
Brui Roman <lateroma@yandex.by>
2023-09-19 18:17:46 +0300

    Добавление столбца «Возраст»

---
+++ ClinicApp/HelperClasses/DateBirthToAgeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace ClinicApp.XamlPages
+{
+    public class DateBirthToAgeConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateOfBirth = (DateTime)value;
+            DateTime todayDateTime = DateTime.Today;
+            int age = todayDateTime.Year - dateOfBirth.Year;
+            if (todayDateTime.Month < dateOfBirth.Month || (todayDateTime.Month == dateOfBirth.Month && todayDateTime.Day < dateOfBirth.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+    }
+}
